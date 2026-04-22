28d6c44ca046f839366b8acfadab118962b91035
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-04-22 19:28:02 -0300

    2 commit tp2

--- practicos/63717 - González, Octavio/tp2/Nodos.cs
+++ practicos/63717 - González, Octavio/tp2/Nodos.cs
@@ -36,4 +36,59 @@ namespace Calculadora
             return x;
         }
     }
+
+    // Esta clase es para operaciones que necesitan dos cosas (izquierda y derecha)
+    public abstract class NodoBinario : Nodo
+    {
+        public Nodo Izquierda;
+        public Nodo Derecha;
+
+        public NodoBinario(Nodo izq, Nodo der)
+        {
+            Izquierda = izq;
+            Derecha = der;
+        }
+    }
+
+    public class SumaNodo : NodoBinario
+    {
+        public SumaNodo(Nodo i, Nodo d) : base(i, d) { }
+        public override int Evaluar(int x)
+        {
+            return Izquierda.Evaluar(x) + Derecha.Evaluar(x);
+        }
+    }
+
+    public class RestaNodo : NodoBinario
+    {
+        public RestaNodo(Nodo i, Nodo d) : base(i, d) { }
+        public override int Evaluar(int x)
+        {
+            return Izquierda.Evaluar(x) - Derecha.Evaluar(x);
+        }
+    }
+
+    public class MultiplicacionNodo : NodoBinario
+    {
+        public MultiplicacionNodo(Nodo i, Nodo d) : base(i, d) { }
+        public override int Evaluar(int x)
+        {
+            return Izquierda.Evaluar(x) * Derecha.Evaluar(x);
+        }
+    }
+
+    public class DivisionNodo : NodoBinario
+    {
+        public DivisionNodo(Nodo i, Nodo d) : base(i, d) { }
+        public override int Evaluar(int x)
+        {
+            int divisor = Derecha.Evaluar(x);
+            if (divisor == 0)
+            {
+                // No se puede dividir por cero, asi que tiramos un error
+                throw new Exception("Error: No podes dividir por cero!");
+            }
+            return Izquierda.Evaluar(x) / divisor;
+        }
+    }
 }

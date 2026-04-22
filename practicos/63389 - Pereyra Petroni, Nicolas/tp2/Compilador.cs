868deec49688abba0827cc80574661cd1c1d1cd2
nicopereyra255-ship-it <nicopereyra255@gmail.com>
2026-04-22 11:50:30 -0300

    Creacion de la clase NumeroNodo
    Creo la clase que representara los valores enteros en el AST

--- practicos/63389 - Pereyra Petroni, Nicolas/tp2/Compilador.cs
+++ practicos/63389 - Pereyra Petroni, Nicolas/tp2/Compilador.cs
@@ -1,5 +1,6 @@
 class Compilador {
     public static Nodo Parse(string expresion) {
         throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
+    fff
     }
 }
--- practicos/63389 - Pereyra Petroni, Nicolas/tp2/Nodos.cs
+++ practicos/63389 - Pereyra Petroni, Nicolas/tp2/Nodos.cs
@@ -1,3 +1,15 @@
 abstract class Nodo {
     public abstract int Evaluar(int x = 0);
 }
+class NumeroNodo : Nodo
+{
+    public int Valor;
+    public NumeroNodo(int valor)
+    {
+        Valor = valor;
+    }
+    public override int Evaluar(int x = 0)
+    {
+        return valor;
+    }
+}

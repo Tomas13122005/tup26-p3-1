bcc86673f71ceb6779834258360dcf989c3f2190
Luciano Perondi <lucianoperondi90@gmail.com>
2026-04-22 18:34:57 -0300

    compilador 2

--- practicos/63354 - Perondi, Luciano/tp2/Compilador.cs
+++ practicos/63354 - Perondi, Luciano/tp2/Compilador.cs
@@ -12,6 +12,33 @@ class Compilador {
         this.posicion = 0;
         this.tokenActual = LeerToken();
     }
+
+    private Token LeerToken() {
+    while (posicion < texto.Length && texto[posicion] == ' ') posicion++;
+
+    if (posicion >= texto.Length) return new Token(TipoToken.Fin);
+
+    char c = texto[posicion++];
+    return c switch {
+        '+' => new Token(TipoToken.suma),
+        '-' => new Token(TipoToken.resta),
+        '*' => new Token(TipoToken.multiplicacion),
+        '/' => new Token(TipoToken.division),
+        '(' => new Token(TipoToken.parentesisAbierto),
+        ')' => new Token(TipoToken.parentesisCerrado),
+        'x' or 'X' => new Token(TipoToken.variable),
+        _ when char.IsDigit(c) => LeerNumero(c),
+        _ => throw new FormatException($"Token inesperado: '{c}'")
+    };
+}
+
+private Token LeerNumero(char primero) {
+    var sb = new StringBuilder();
+    sb.Append(primero);
+    while (posicion < texto.Length && char.IsDigit(texto[posicion]))
+        sb.Append(texto[posicion++]);
+    return new Token(TipoToken.Numero, int.Parse(sb.ToString()));
+}
 }
 
 

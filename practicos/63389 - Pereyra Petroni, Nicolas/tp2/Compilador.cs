ec51c0af44b28a96cca565bfca1af767c39f5c02
nicopereyra255-ship-it <nicopereyra255@gmail.com>
2026-04-22 15:45:11 -0300

    clase compilador y parser
    Agrega Compilador.Parse y estructura base del Parser para inicio del análisis sintáctico

--- practicos/63389 - Pereyra Petroni, Nicolas/tp2/Compilador.cs
+++ practicos/63389 - Pereyra Petroni, Nicolas/tp2/Compilador.cs
@@ -1,6 +1,23 @@
-class Compilador {
-    public static Nodo Parse(string expresion) {
-        throw new NotImplementedException("Implementar el parser para convertir la expresión en un AST.");
-    fff
+
+class Compilador 
+{
+    public static Nodo Parse(string expresion) 
+    {
+     var parser = new Parser(expresion);
+     return parser.Parsear();
+    }
+}
+class Parser
+{
+    private string texto;
+    private int pos;
+    public Parser (string texto)
+    {
+        this.texto = texto.Replace(" ", "");
+        this.pos = 0;
+    }
+    public  Nodo Parsear()
+    {
+        return ParseExpresion();
     }
 }

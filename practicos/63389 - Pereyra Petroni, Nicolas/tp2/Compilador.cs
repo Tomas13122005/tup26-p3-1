84251629121e37da3b43fe3bcd9b666a9c62d1e8
nicopereyra255-ship-it <nicopereyra255@gmail.com>
2026-04-22 15:55:29 -0300

    ParseExpresion y metodo Match
    Implementa ParseExpresion y método Match para manejar suma y resta en el parser

--- practicos/63389 - Pereyra Petroni, Nicolas/tp2/Compilador.cs
+++ practicos/63389 - Pereyra Petroni, Nicolas/tp2/Compilador.cs
@@ -1,23 +1,60 @@
-
 class Compilador 
 {
     public static Nodo Parse(string expresion) 
     {
-     var parser = new Parser(expresion);
-     return parser.Parsear();
+        var parser = new Parser(expresion);
+        return parser.Parsear();
     }
 }
+
 class Parser
 {
     private string texto;
     private int pos;
-    public Parser (string texto)
+
+    public Parser(string texto)
     {
         this.texto = texto.Replace(" ", "");
         this.pos = 0;
     }
-    public  Nodo Parsear()
+
+    public Nodo Parsear()
     {
         return ParseExpresion();
+    }
+
+    
+
+    private Nodo ParseExpresion()
+    {
+        Nodo nodo = ParseTermino();
+
+        while (true)
+        {
+            if (Match('+'))
+            {
+                nodo = new SumaNodo(nodo, ParseTermino());
+            }
+            else if (Match('-'))
+            {
+                nodo = new RestaNodo(nodo, ParseTermino());
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return nodo;
+    }
+
+    private bool Match(char c)
+    {
+        if (pos < texto.Length && texto[pos] == c)
+        {
+            pos++;
+            return true;
+        }
+        return false;
     }
 }

93027bd0c629f7c1ba49ee239a480b1bd8439c33
Octavio Gonzalez <octaviofacultad@gmail.com>
2026-04-22 23:21:33 -0300

    commit tp2

--- practicos/63717 - González, Octavio/tp2/Compilador.cs
+++ practicos/63717 - González, Octavio/tp2/Compilador.cs
@@ -1,103 +1,252 @@
 using System;
 using System.Collections.Generic;
 
-namespace Calculadora
+// Aca pongo los tipos de cosas que podemos encontrar en la cuenta
+public enum TipoToken
 {
-    // Aca pongo los tipos de cosas que podemos encontrar en la cuenta
-    public enum TipoToken
+    Numero,
+    Suma,
+    Resta,
+    Multiplicacion,
+    Division,
+    ParentesisIzquierdo,
+    ParentesisDerecho,
+    VariableX,
+    FinDeLinea // Para saber cuando se termina la formula
+}
+
+// Esta clase guarda que tipo es y si tiene algun valor (como los numeros)
+public class Token
+{
+    public TipoToken Tipo;
+    public string Valor;
+
+    public Token(TipoToken tipo, string valor = "")
     {
-        Numero,
-        Suma,
-        Resta,
-        Multiplicacion,
-        Division,
-        ParentesisIzquierdo,
-        ParentesisDerecho,
-        VariableX,
-        FinDeLinea // Para saber cuando se termina la formula
+        Tipo = tipo;
+        Valor = valor;
+    }
+}
+
+// Esta clase es la que lee la formula y la separa en partes (tokens)
+public class Lexer
+{
+    private string _texto;
+    private int _posicion;
+
+    public Lexer(string texto)
+    {
+        _texto = texto;
+        _posicion = 0;
     }
 
-    // Esta clase guarda que tipo es y si tiene algun valor (como los numeros)
-    public class Token
+    public List<Token> Escanear()
     {
-        public TipoToken Tipo;
-        public string Valor;
+        List<Token> tokens = new List<Token>();
 
-        public Token(TipoToken tipo, string valor = "")
+        // Si el texto esta vacio o es solo espacios
+        if (string.IsNullOrWhiteSpace(_texto))
         {
-            Tipo = tipo;
-            Valor = valor;
-        }
-    }
-
-    // Esta clase es la que lee la formula y la separa en partes (tokens)
-    public class Lexer
-    {
-        private string _texto;
-        private int _posicion;
-
-        public Lexer(string texto)
-        {
-            _texto = texto;
-            _posicion = 0;
+            throw new FormatException("Token inesperado: entrada vacia");
         }
 
-        public List<Token> Escanear()
+        while (_posicion < _texto.Length)
         {
-            List<Token> tokens = new List<Token>();
+            char actual = _texto[_posicion];
 
-            while (_posicion < _texto.Length)
+            // Si es un espacio, lo saltamos y seguimos
+            if (char.IsWhiteSpace(actual))
             {
-                char actual = _texto[_posicion];
-
-                // Si es un espacio, lo saltamos y seguimos
-                if (char.IsWhiteSpace(actual))
-                {
-                    _posicion++;
-                    continue;
-                }
-
-                // Si es una x (o X), guardamos el token de variable
-                if (actual == 'x' || actual == 'X')
-                {
-                    tokens.Add(new Token(TipoToken.VariableX, "x"));
-                    _posicion++;
-                    continue;
-                }
-
-                // Si es un numero, leemos todos los digitos que vengan juntos
-                if (char.IsDigit(actual))
-                {
-                    string numero = "";
-                    while (_posicion < _texto.Length && char.IsDigit(_texto[_posicion]))
-                    {
-                        numero = numero + _texto[_posicion];
-                        _posicion++;
-                    }
-                    tokens.Add(new Token(TipoToken.Numero, numero));
-                    continue;
-                }
-
-                // Aca miramos los signos uno por uno
-                switch (actual)
-                {
-                    case '+': tokens.Add(new Token(TipoToken.Suma)); break;
-                    case '-': tokens.Add(new Token(TipoToken.Resta)); break;
-                    case '*': tokens.Add(new Token(TipoToken.Multiplicacion)); break;
-                    case '/': tokens.Add(new Token(TipoToken.Division)); break;
-                    case '(': tokens.Add(new Token(TipoToken.ParentesisIzquierdo)); break;
-                    case ')': tokens.Add(new Token(TipoToken.ParentesisDerecho)); break;
-                    default:
-                        // Si no es nada de lo anterior, recien ahi tiramos error
-                        throw new Exception("No entiendo que es este simbolo: " + actual);
-                }
-
                 _posicion++;
+                continue;
             }
 
-            // Al final de todo avisamos que terminamos
-            tokens.Add(new Token(TipoToken.FinDeLinea));
-            return tokens;
+            // Si es una x (o X), guardamos el token de variable
+            if (actual == 'x' || actual == 'X')
+            {
+                tokens.Add(new Token(TipoToken.VariableX, "x"));
+                _posicion++;
+                continue;
+            }
+
+            // Si es un numero, leemos todos los digitos que vengan juntos
+            if (char.IsDigit(actual))
+            {
+                string numero = "";
+                while (_posicion < _texto.Length && char.IsDigit(_texto[_posicion]))
+                {
+                    numero = numero + _texto[_posicion];
+                    _posicion++;
+                }
+                tokens.Add(new Token(TipoToken.Numero, numero));
+                continue;
+            }
+
+            // Aca miramos los signos uno por uno
+            switch (actual)
+            {
+                case '+': tokens.Add(new Token(TipoToken.Suma)); break;
+                case '-': tokens.Add(new Token(TipoToken.Resta)); break;
+                case '*': tokens.Add(new Token(TipoToken.Multiplicacion)); break;
+                case '/': tokens.Add(new Token(TipoToken.Division)); break;
+                case '(': tokens.Add(new Token(TipoToken.ParentesisIzquierdo)); break;
+                case ')': tokens.Add(new Token(TipoToken.ParentesisDerecho)); break;
+                default:
+                    // Si no es nada de lo anterior, usamos la excepcion que pide el test
+                    throw new FormatException("Token inesperado: " + actual);
+            }
+
+            _posicion++;
         }
+
+        // Al final de todo avisamos que terminamos
+        tokens.Add(new Token(TipoToken.FinDeLinea));
+        return tokens;
+    }
+}
+
+// Esta clase es la que arma el arbol (AST) usando los tokens que nos da el Lexer
+public class Parser
+{
+    private List<Token> _tokens;
+    private int _indiceActual;
+
+    public Parser(List<Token> tokens)
+    {
+        _tokens = tokens;
+        _indiceActual = 0;
+    }
+
+    // Para ver que token sigue sin mover el indice
+    private Token TokenActual
+    {
+        get { return _tokens[_indiceActual]; }
+    }
+
+    // Para avanzar al siguiente token
+    private void Avanzar()
+    {
+        if (_indiceActual < _tokens.Count - 1)
+        {
+            _indiceActual++;
+        }
+    }
+
+    public Nodo Parsear()
+    {
+        Nodo resultado = Expresion();
+
+        if (TokenActual.Tipo != TipoToken.FinDeLinea)
+        {
+             throw new FormatException("Token inesperado al final");
+        }
+        
+        return resultado;
+    }
+
+    // Aca manejamos las sumas y restas
+    private Nodo Expresion()
+    {
+        Nodo izquierda = Termino();
+
+        while (TokenActual.Tipo == TipoToken.Suma || TokenActual.Tipo == TipoToken.Resta)
+        {
+            TipoToken tipo = TokenActual.Tipo;
+            Avanzar();
+            Nodo derecha = Termino();
+
+            if (tipo == TipoToken.Suma)
+            {
+                izquierda = new SumaNodo(izquierda, derecha);
+            }
+            else
+            {
+                izquierda = new RestaNodo(izquierda, derecha);
+            }
+        }
+
+        return izquierda;
+    }
+
+    // Aca van las multiplicaciones y divisiones
+    private Nodo Termino()
+    {
+        Nodo izquierda = Factor();
+
+        while (TokenActual.Tipo == TipoToken.Multiplicacion || TokenActual.Tipo == TipoToken.Division)
+        {
+            TipoToken tipo = TokenActual.Tipo;
+            Avanzar();
+            Nodo derecha = Factor();
+
+            if (tipo == TipoToken.Multiplicacion)
+            {
+                izquierda = new MultiplicacionNodo(izquierda, derecha);
+            }
+            else
+            {
+                izquierda = new DivisionNodo(izquierda, derecha);
+            }
+        }
+
+        return izquierda;
+    }
+
+    // Aca manejamos numeros, x, parentesis y signos unarios (+ y -)
+    private Nodo Factor()
+    {
+        if (TokenActual.Tipo == TipoToken.Suma)
+        {
+            Avanzar();
+            return Factor();
+        }
+
+        if (TokenActual.Tipo == TipoToken.Resta)
+        {
+            Avanzar();
+            return new NegativoNodo(Factor());
+        }
+
+        if (TokenActual.Tipo == TipoToken.ParentesisIzquierdo)
+        {
+            Avanzar();
+            Nodo nodo = Expresion();
+
+            if (TokenActual.Tipo != TipoToken.ParentesisDerecho)
+            {
+                // El mensaje que espera la prueba de Octavio
+                throw new FormatException("Se esperaba ')'");
+            }
+            Avanzar();
+            return nodo;
+        }
+
+        if (TokenActual.Tipo == TipoToken.Numero)
+        {
+            int valor = int.Parse(TokenActual.Valor);
+            Avanzar();
+            return new NumeroNodo(valor);
+        }
+
+        if (TokenActual.Tipo == TipoToken.VariableX)
+        {
+            Avanzar();
+            return new VariableNodo();
+        }
+
+        throw new FormatException("Token inesperado");
+    }
+}
+
+// Una clase de ayuda para usar el compilador mas facil
+public class Compilador
+{
+    
+    public static Nodo Parse(string formula)
+    {
+        Lexer lexer = new Lexer(formula);
+        List<Token> tokens = lexer.Escanear();
+        Parser parser = new Parser(tokens);
+        return parser.Parsear();
     }
 }
--- practicos/63717 - González, Octavio/tp2/Nodos.cs
+++ practicos/63717 - González, Octavio/tp2/Nodos.cs
@@ -1,94 +1,107 @@
 using System;
 
-namespace Calculadora
+// Esta es la clase padre de todos los nodos
+public abstract class Nodo
 {
-    // Esta es la clase padre de todos los nodos
-    public abstract class Nodo
+    // Todos los nodos tienen que tener este metodo para sacar la cuenta
+    // Si no le pasamos x, vale 0 por defecto
+    public abstract int Evaluar(int x = 0);
+}
+
+// Este nodo es para cuando hay un numero solo, como el 5 o el 10
+public class NumeroNodo : Nodo
+{
+    public int MiNumero;
+
+    public NumeroNodo(int numero)
     {
-        // Todos los nodos tienen que tener este metodo para sacar la cuenta
-        // Si no le pasamos x, vale 0 por defecto
-        public abstract int Evaluar(int x = 0);
+        MiNumero = numero;
     }
 
-    // Este nodo es para cuando hay un numero solo, como el 5 o el 10
-    public class NumeroNodo : Nodo
+    public override int Evaluar(int x)
     {
-        public int MiNumero;
+        // Aca solo devolvemos el numero que guardamos
+        return MiNumero;
+    }
+}
 
-        public NumeroNodo(int numero)
-        {
-            MiNumero = numero;
-        }
+// Este es para cuando aparece la letra x en la cuenta
+public class VariableNodo : Nodo
+{
+    public override int Evaluar(int x)
+    {
+        // Devolvemos el valor de x que nos pasaron
+        return x;
+    }
+}
 
-        public override int Evaluar(int x)
-        {
-            // Aca solo devolvemos el numero que guardamos
-            return MiNumero;
-        }
+// Este es para cuando hay un signo menos adelante, como -5 o -(x+2)
+public class NegativoNodo : Nodo
+{
+    public Nodo Hijo;
+
+    public NegativoNodo(Nodo h)
+    {
+        Hijo = h;
     }
 
-    // Este es para cuando aparece la letra x en la cuenta
-    public class VariableNodo : Nodo
+    public override int Evaluar(int x)
     {
-        public override int Evaluar(int x)
-        {
-            // Devolvemos el valor de x que nos pasaron
-            return x;
-        }
+        // Evaluamos lo de adentro y le cambiamos el signo
+        return -Hijo.Evaluar(x);
     }
+}
 
-    // Esta clase es para operaciones que necesitan dos cosas (izquierda y derecha)
-    public abstract class NodoBinario : Nodo
+// Esta clase es para operaciones que necesitan dos cosas (izquierda y derecha)
+public abstract class NodoBinario : Nodo
+{
+    public Nodo Izquierda;
+    public Nodo Derecha;
+
+    public NodoBinario(Nodo izq, Nodo der)
     {
-        public Nodo Izquierda;
-        public Nodo Derecha;
-
-        public NodoBinario(Nodo izq, Nodo der)
-        {
-            Izquierda = izq;
-            Derecha = der;
-        }
+        Izquierda = izq;
+        Derecha = der;
     }
+}
 
-    public class SumaNodo : NodoBinario
+public class SumaNodo : NodoBinario
+{
+    public SumaNodo(Nodo i, Nodo d) : base(i, d) { }
+    public override int Evaluar(int x)
     {
-        public SumaNodo(Nodo i, Nodo d) : base(i, d) { }
-        public override int Evaluar(int x)
-        {
-            return Izquierda.Evaluar(x) + Derecha.Evaluar(x);
-        }
+        return Izquierda.Evaluar(x) + Derecha.Evaluar(x);
     }
+}
 
-    public class RestaNodo : NodoBinario
+public class RestaNodo : NodoBinario
+{
+    public RestaNodo(Nodo i, Nodo d) : base(i, d) { }
+    public override int Evaluar(int x)
     {
-        public RestaNodo(Nodo i, Nodo d) : base(i, d) { }
-        public override int Evaluar(int x)
-        {
-            return Izquierda.Evaluar(x) - Derecha.Evaluar(x);
-        }
+        return Izquierda.Evaluar(x) - Derecha.Evaluar(x);
     }
+}
 
-    public class MultiplicacionNodo : NodoBinario
+public class MultiplicacionNodo : NodoBinario
+{
+    public MultiplicacionNodo(Nodo i, Nodo d) : base(i, d) { }
+    public override int Evaluar(int x)
     {
-        public MultiplicacionNodo(Nodo i, Nodo d) : base(i, d) { }
-        public override int Evaluar(int x)
-        {
-            return Izquierda.Evaluar(x) * Derecha.Evaluar(x);
-        }
+        return Izquierda.Evaluar(x) * Derecha.Evaluar(x);
     }
+}
 
-    public class DivisionNodo : NodoBinario
+public class DivisionNodo : NodoBinario
+{
+    public DivisionNodo(Nodo i, Nodo d) : base(i, d) { }
+    public override int Evaluar(int x)
     {
-        public DivisionNodo(Nodo i, Nodo d) : base(i, d) { }
-        public override int Evaluar(int x)
+        int divisor = Derecha.Evaluar(x);
+        if (divisor == 0)
         {
-            int divisor = Derecha.Evaluar(x);
-            if (divisor == 0)
-            {
-                // No se puede dividir por cero, asi que tiramos un error
-                throw new Exception("Error: No podes dividir por cero!");
-            }
-            return Izquierda.Evaluar(x) / divisor;
+            throw new DivideByZeroException("Error: No podes dividir por cero!");
         }
+        return Izquierda.Evaluar(x) / divisor;
     }
 }
